fe6c763a23822b84e3ed1c38bb41b21ad92d98ef
gjarchila <techsuppty@gmail.com>
2023-04-04 18:27:08 +0000

    [ADD] Basic Module

--- odoo_academy/controllers/academy_controllers.py
+++ odoo_academy/controllers/academy_controllers.py
@@ -10,13 +10,14 @@ class Academy(http.Controller):
     @http.route('/academy/courses/', auth='public', website=True)
     def courses(self, **kw):
         courses = http.request.env['academy.course'].search([])
-        return http.request.render('odoo.academy.course_website', {
-            'courses':courses, 
+        return http.request.render('odoo_academy.course_website', {
+            'courses': courses, 
         })
     
     @http.route('/academy/<model("academy.session"):session>/', auth='public', website=True)
     def session(self, session):
-        return http.request.render('odoo.academy.session_website', {
+        return http.request.render('odoo_academy.session_website', {
+            'session': session,
         })
     
     
